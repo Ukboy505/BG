851eb01fdc5b16d641bc32cb8d19f8b944db48c7
Ukboy505 <ukashatuabdullah505@gmail.com>
2025-06-18 15:02:59 +0100

    Update ui.js

--- ui.js
+++ ui.js
@@ -1,7 +1,7 @@
 // js/ui.js
 import { generateTradingSignal } from './signals.js';
 import { updateChartWithSignal } from './chart.js';
-import { currentCandles } from './data.js';
+import { initialState, currentCandles } from './data.js';
 import { currentTimezone, formatUnixTimestamp } from './utils.js';
 
 export function updateWithSignal(signalResult, isCached = false, isRestored = false) {
